b44c671800120f3aa9f8d66924b1e911ff7ddbf6
saumya-bhagat <saumyabhagat7@gmail.com>
2020-04-28 16:02:59 -0400

    changes

--- src/gpgpu-sim/shader.cc
+++ src/gpgpu-sim/shader.cc
@@ -2449,11 +2449,11 @@ void ldst_unit::cycle() {
             address_type chosen_tag;
             m_L1D->fill(mf, m_core->get_gpu()->gpu_sim_cycle +
                                 m_core->get_gpu()->gpu_tot_sim_cycle, evicted_index, evicted_tag);
-            chosen_way = m_vtt->get_way(evicted_index);
+           /* chosen_way = m_vtt->get_way(evicted_index);
             m_vtt->fill_tag(evicted_tag, evicted_index);
             chosen_tag = m_vtt->m_vtt_entry[evicted_index][chosen_way].tag;
             printf("Evicted_tag = %x, VTT[%lu][%lu] = %x", evicted_tag, evicted_index, chosen_way,chosen_tag);
-            
+            */
             m_response_fifo.pop_front();
           }
         }
