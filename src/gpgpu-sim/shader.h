4d4cf8f04848b986802ec89de7c30360e109440c
Raghav Venkatasubramanian <v.raghav295@gmail.com>
2020-05-06 01:14:47 -0400

    kmeans

--- src/gpgpu-sim/shader.h
+++ src/gpgpu-sim/shader.h
@@ -2281,7 +2281,7 @@ class perfect_memory_interface : public mem_fetch_interface {
   simt_core_cluster *m_cluster;
 };
 
-#define MONITORING_PERIOD 500
+#define MONITORING_PERIOD 1000
 #define NUM_PERIODS 2
 inline int scheduler_unit::get_sid() const { return m_shader->get_sid(); }
 //stats for various linebacker components
@@ -2353,7 +2353,7 @@ class victim_tag_table {
 
 ////////LOAD MONITOR///////
 #define LOAD_MONITOR_ENTRIES 32
-#define HIT_THRESHOLD 0
+#define HIT_THRESHOLD 150
 struct load_monitor_entry {
 
   address_type PC;
